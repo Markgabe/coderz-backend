07b783c6a6605216340dc62761611922a909901e
Mark <marcos98cf@gmail.com>
2021-11-05 11:57:30 -0300

    Adding cert validation

--- app/index.js
+++ app/index.js
@@ -12,7 +12,13 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
-app.get('/.well-known/pki-validation/godaddy.html', (req, res) => { res.send('sndiv9ldc27v0vg1r8eka8lode'); });
+app.get('/.well-known/pki-validation/A37BA752A9687A1E1947DCD40C9400EF.txt', (req, res) => {
+  res.send(
+    `5BDF29BFFB5FD05117673C7FCF437827D8C72739FF35E12920962A2FA06CEC56
+comodoca.com
+32dc348b9b4fea2`,
+  );
+});
 app.use(router);
 
 app.listen(process.env.PORT || 3333);

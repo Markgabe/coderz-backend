cd14ce8c2d42465eba9c7c11ebb22e6c86b1dafe
Mark <marcos98cf@gmail.com>
2021-11-04 22:47:59 -0300

    Passing env secrets to application

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: 'coderz',
-  host: '127.0.0.1',
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 };

efdd211cc8d84ac3fd706d21bdc7750d08c0d09f
Marcos Paulo <mgabriel@id.uff.br>
2021-11-16 20:05:10 -0300

    Updated configs

--- app/models/index.js
+++ app/models/index.js
@@ -14,6 +14,15 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+sequelize
+    .authenticate()
+    .then(function(_err) {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(function (err) {
+        console.log('Unable to connect to the database:', err);
+});
+
 fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))

89cb232323cfa57d9cf721f83db0f7b38e165e18
Mark <marcos98cf@gmail.com>
2021-11-18 20:03:34 -0300

    config: Changed database to mariadb

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: 'postgres',
+  dialect: 'mariadb',
   dialectOptions: {
     encrypt: true,
   },
